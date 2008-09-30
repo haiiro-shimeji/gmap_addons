d7ee4489e37698e422d7972b151c17468c9d6834
Brandon Bergren <bdragon@rtk0.net>
2008-09-30 20:46:58 +0000

    gmap_debug Drupal 6 compatibility.

--- gmap_debug/gmap_debug.js
+++ gmap_debug/gmap_debug.js
@@ -1,18 +1,17 @@
 // $Id$
 
-if (Drupal.jsEnabled) {
-  $(document).ready(function() {
-
-    $('#gmap-debug-forcesizecheck').click(function(e) {
+Drupal.behaviors.gmap_debug = function (context) {
+  $('#gmap-debug-forcesizecheck:not(.gmap-debug-processed)', context).addClass('gmap-debug-processed').each(function () {
+    $(this).click(function(e) {
       e.preventDefault();
       if (Drupal.gmap) {
         Drupal.gmap.globalChange('widthchange');
-        alert('Dimensions recalculated.');
+        alert(Drupal.t('Dimensions recalculated.'));
       }
       else {
-        alert('No maps loaded.');
+        alert(Drupal.t('No maps loaded.'));
       }
       return false;
     });
   });
-}
+};
