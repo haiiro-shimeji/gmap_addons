0f535b314f2a8c8618cbd19e896ed094b445420e
Brandon Bergren <bdragon@rtk0.net>
2008-10-01 21:19:03 +0000

    Add a second link to reboot a map using the function I just added to gmap.

--- gmap_debug/gmap_debug.js
+++ gmap_debug/gmap_debug.js
@@ -14,4 +14,23 @@ Drupal.behaviors.gmap_debug = function (context) {
       return false;
     });
   });
+  $('#gmap-debug-reboot:not(.gmap-debug-processed)', context).addClass('gmap-debug-processed').each(function () {
+    $(this).click(function(e) {
+      e.preventDefault();
+      var reply = prompt('Map ID to reboot?');
+      if (Drupal.gmap && Drupal.gmap.getMap(reply)) {
+        Drupal.gmap.unloadMap(reply);
+        var ctr = 0;
+        while ($('#gmap-'+reply+'-gmap'+ctr)[0]) {
+          $('#gmap-'+reply+'-gmap'+ctr).removeClass('gmap-processed').empty().append('REBOOTING...');
+          ctr = ctr + 1;
+        }
+        Drupal.attachBehaviors(document);
+      }
+      else {
+        alert(Drupal.t('Unable to locate requested map.'));
+      }
+      return false;
+    });
+  });
 };
