00691ade83694738331fe218c21dc53e4dd75d60
Brandon Bergren <bdragon@rtk0.net>
2011-01-26 01:52:52 +0000

    Fix #341589, reported by wessex, patch by geodaniel: Remove debugging alert() that has been in the code for two years. X(

--- gmap_overlays/js/overlay_geoxml.js
+++ gmap_overlays/js/overlay_geoxml.js
@@ -8,9 +8,6 @@ Drupal.gmap.clientsidexml = {};
 Drupal.gmap.addHandler('gmap', function(elem) {
   var obj = this;
   obj.bind('init', function() {
-
-  alert (obj.map.getContainer().id);
-
     $.each(obj.vars.overlay, function(i,d) {
       switch (d.type) {
         case 'georss':
